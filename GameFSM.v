2b9c0c6955c0e18a05cf2cc07221c0eba16756c5
CodyCao <cody.cao26@gmail.com>
2016-11-25 21:32:39 -0800

    v1.1 of completed FSM

--- GameFSM.v
+++ GameFSM.v
@@ -23,24 +23,25 @@
 module GameFSM(
     input clk,
     input reset,
-    input levelPassed,            // If player passes level
-    output reg [2:0] level,
-    output reg world
+    input levelPassed,              // If player passes level
+    input lose,                      // If player losses
+    output reg [2:0] level,         // Current game level
+    output reg world,               // Current game world
+    output reg [2:0] gameStatus           // 0 = playing, 1 = level win, 2 = world win, 3 = Game win, 4 = gameOver
     );
     
     reg [2:0] currentState;
     reg [2:0] nextState;
     
-    reg levelPassedToggle;  //  ensures levelPassed resets to zero. 
     reg levelTemp;       // Temporary level reg for incrementation.
     
     parameter initialState = 0;
-    parameter World1 = 1;
-    parameter World2 = 2;
+    parameter World0 = 1;
+    parameter World1 = 2;
     parameter winLevel = 3;
     parameter winWorld = 4;
     parameter winGame = 5;
-    parameter Lose = 6;
+    parameter gameOver = 6;
     
     parameter maxLevel = 5;
     
@@ -56,32 +57,65 @@ module GameFSM(
     
     always @ (*) begin
         nextState  <= currentState;
-        world <= 0;
         levelTemp <= level;
         case(currentState)
             initialState: begin
                 // Set any Initial values here
+                world <= 0;
+                gameStatus <= 0;
                 level <= 0;
                 levelTemp <= 0;
                 nextState <= World1;
             end
-            World1: begin
+            World0: begin
                 world <= 0;
+                gameStatus <= 0;
                 if(levelPassed) begin
                     level <= levelTemp + 1; // increment level
                     if (level == maxLevel) begin
                         level <= 0;
-                        nextState <= World2;
+                        nextState <= winWorld;
                     end
                     else nextState <= winLevel;
                 end
-                else if() begin
-                
-                end
-                
-                
+                else if (lose) nextState <= gameOver;             
             end
-            
+            World1: begin
+                world <= 1;
+                gameStatus <= 0;
+                if(levelPassed) begin
+                    level <= levelTemp + 1; // increment level
+                    if (level == maxLevel) begin
+                        level <= 0;
+                        nextState <= winWorld;
+                    end
+                    else nextState <= winLevel;
+                end
+                else if(lose) nextState <= gameOver;
+            end
+            winLevel: begin
+                // Win level sequence
+                gameStatus <= 1;
+                if (!levelPassed && world == 0)nextState <= World0;
+                else if (!levelPassed && world == 1)nextState <= World1;
+
+            end
+            winWorld: begin
+                // Win World Sequence                 
+                gameStatus <= 2;                
+                if (!levelPassed) nextState <=  World1;     // May need altering, depending on number of worlds.
+                else if(!levelPassed) nextState <= winGame;
+            end
+            winGame: begin
+                // Win Game Sequence
+                gameStatus <= 3;
+                // nextState?
+            end
+            gameOver: begin
+                // Lose Sequenece
+                gameStatus <= 4;
+                // nextState?
+            end            
         endcase
     end
     
