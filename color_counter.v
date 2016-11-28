b6ffcb44cafc963089022f26ee03744bd48bb580
Kebba Kanyi <kebbakanyi@gmail.com>
2016-11-27 20:30:55 -0800

    counter

---
+++ color_counter.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/27/2016 05:31:51 PM
+// Design Name: 
+// Module Name: color_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module color_counter(
+    input clk,
+    output reg [3:0] color
+    );
+    
+    reg [1:0] count = 0;
+    
+    always @ (posedge clk) begin
+        count <= count +1;
+        if(count == 1)begin
+        
+            color <=2;
+            
+        end
+        
+        else if (count ==2) begin
+       
+            color <=3; 
+              
+        end
+        
+        else if (count ==3) begin
+       
+            color <=4; 
+              
+        end
+
+        else if (count ==4) begin
+       
+            color <=5; 
+              
+        end
+        
+        else begin
+            count <= 0;
+        end
+        
+       
+    end
+    
+endmodule
